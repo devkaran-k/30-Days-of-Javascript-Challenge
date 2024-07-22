ca5b111916577a5a36a4602b00dcf483bb24b61e
devkaran-k <137026533+devkaran-k@users.noreply.github.com>
2024-07-22 14:25:42 +0530

    asd

---
+++ Day_2_Operators/02_day.js
@@ -0,0 +1,60 @@
+// Day 2: Operators
+// Tasks/Activities:
+
+
+console.log("Activity 1: Arithmetic Operations");
+
+// Task 1: Write a program to add two numbers and log the result to the console.
+const addTwo = 1 + 7;
+console.log(addTwo);
+
+// Task 2: Write a program to subtract two numbers and log the result to the console.
+
+const minusTwo = 8 - 1;
+console.log(minusTwo);
+
+// Task 3: Write a program to multiply two numbers and log the result to the console.
+const multiNumber = 9*9;
+console.log(multiNumber);
+
+// Task 4: Write a program to divide two numbers and log the result to the console.
+
+const divideNumber = 369 / 3;
+console.log(divideNumber);
+
+// Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+
+const reminderNum = 13 % 7;
+console.log(reminderNum);
+
+console.log("Activity 2: Assignment Operators");
+
+// Task 6: Use the += operator to add a number to a variable and log the result to the console.
+
+let assignNum = 234;
+assignNum += 6;
+
+console.log(assignNum);
+
+// Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
+
+let assignSubtract = 369;
+assignSubtract -= 9;
+
+console.log(assignSubtract);
+
+console.log("Activity 3: Comparison Operators");
+// Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+
+const compNum = 234 != 235;
+console.log(compNum);
+
+let a = [45, 56, 89, 78];
+let b = ["ab", "cd", "ef", "gh", "hi"];
+const compareG = a.length < b.length;
+
+console.log(compareG);
+// Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
+
+let strNum = "devkaran" >= 
+// Task 10: Write a program to compare two numbers using == and === and log the result to the console.
